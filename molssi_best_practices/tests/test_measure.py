c106d55cd2b9dc806cdfd8f5256e3b5fc2140fa0
david poole <dpoole34@gatech.edu>
2023-07-19 10:42:27 -0400

    Add and refine some tests

---
+++ molssi_best_practices/tests/test_measure.py
@@ -0,0 +1,20 @@
+"""
+Unit tests for the measure.py module 
+"""
+
+import numpy as np
+import pytest
+
+import molssi_best_practices as mbp
+
+@pytest.mark.slow
+def test_calculate_distance():
+    """Test computing distance between two points"""
+    rA = np.array([0, 0, 0])
+    rB = np.array([0, 0, 1])
+
+    expected_distance = 1.0 
+
+    calculated_distance = mbp.calculate_distance(rA, rB)
+
+    assert expected_distance == calculated_distance
--- molssi_best_practices/tests/test_molssi_best_practices.py
+++ molssi_best_practices/tests/test_molssi_best_practices.py
@@ -3,13 +3,21 @@ Unit and regression test for the molssi_best_practices package.
 """
 
 # Import package, test suite, and other packages as needed
+import numpy as np
+import pytest
 import sys
 
-import pytest
-
-import molssi_best_practices
+import molssi_best_practices as mbp
 
 
 def test_molssi_best_practices_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "molssi_best_practices" in sys.modules
+
+
+def test_build_bond_list_exception():
+    """Make sure the code throws correctly on nonsensical user inputs."""
+    coordinates = np.array([[0, 0, 0], [0, 1, 0]])
+
+    with pytest.raises(ValueError) as e_info:
+        mbp.build_bond_list(coordinates, max_bond=1.5, min_bond=2)
